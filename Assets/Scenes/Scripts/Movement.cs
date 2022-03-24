07deadb71c47a9fdfa127db222e2950f81364677
Mestaren <lucas.svensson@elev.ga.lbs.se>
2022-03-24 14:36:42 +0100

    movement part 1
    skickar till isac

---
+++ Assets/Scenes/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float MoveSpeed = 6f;
+
+    float Horizontalmove;
+    float VerticalMove;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    private void update()
+    {
+        MyInput();
+    }
+
+    void MyInput()
+    {
+        Horizontalmove = Input.GetAxisRaw("Horizontal");
+        VerticalMove = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * VerticalMove + transform.right * Horizontalmove;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * MoveSpeed, ForceMode.Acceleration);
+    }
+}
